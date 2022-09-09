e9e95dc939f185a0e119223671b6a4d009907b87
GlebTc <bob88tikhonov@gmail.com>
2022-09-08 21:48:37 -0400

    Covered basic psq functionality.

--- DB/db.sql
+++ DB/db.sql
@@ -1,5 +1,10 @@
 -- Type out comands here and then paste in terminal
 
 CREATE TABLE restaurants (
-    id
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
 );
+
+INSERT INTO restaurants (name, location, price_range) values ('wendys', 'denver', 3);
