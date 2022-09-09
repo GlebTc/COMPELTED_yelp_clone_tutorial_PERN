40a6f606ebe24f13de4706a9a08c936731eec38f
GlebTc <bob88tikhonov@gmail.com>
2022-09-09 00:52:25 -0400

    Starting midleware

--- server/server.js
+++ server/server.js
@@ -4,7 +4,28 @@ const express = require ('express')
 
 const app = express()
 
-const port = 3005
+// Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ['mcdonalds', 'wendys']
+        }
+        
+    })
+})
+
+// Get individualrestaurant
+app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+    console.log(req.params)
+})
+
+// Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req)
+})
+
+const port = process.env.PORT || 3001;
 
 
 app.listen(port, () => {
