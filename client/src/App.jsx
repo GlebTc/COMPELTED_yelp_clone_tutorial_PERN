3927d80b60500b0bd1aec8c072b0299f70d53aec
GlebTc <bob88tikhonov@gmail.com>
2022-09-12 23:18:08 -0400

    Data rendered through api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UpdatePage from './routes/UpdatePage'
 import RestaurantDetailPage from './routes/RestaurantDetailPage'
 import Home from './routes/Home'
+import { RestaurantContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
     return (
-        <div className="container">
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
-                    <Route exact path="/restaurants/:id" element={<RestaurantDetailPage />} />
-                </Routes>
-            </Router>
-        </div>
+        <RestaurantContextProvider>
+            <div className="container">
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
+                        <Route exact path="/restaurants/:id" element={<RestaurantDetailPage />} />
+                    </Routes>
+                </Router>
+            </div>
+        </RestaurantContextProvider>
     )
 }
 
