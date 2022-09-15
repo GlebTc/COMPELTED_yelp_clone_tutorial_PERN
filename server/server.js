197eab438d23ba9b6097cb02509bd366dac99599
GlebTc <bob88tikhonov@gmail.com>
2022-09-15 11:09:52 -0400

    Completed addReview backend and functionality

--- server/server.js
+++ server/server.js
@@ -106,8 +106,23 @@ app.delete('/api/v1/restaurants/:id', async (req,res) => {
 
 })
 
-const port = process.env.PORT || 3001;
+app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
 
+    try {
+        const newReview = await db.query(
+            "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *", [req.params.id, req.body.name, req.body.review, req.body.rating]);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                review: newReview.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`server is up and listeneing on port ${port}`)
